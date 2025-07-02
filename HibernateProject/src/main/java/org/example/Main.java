f45f745151b9b15c4948ce42a80d695ec58c00d6
Ritwik <ritwikhero@gmail.com>
2025-07-03 02:08:15 +0530

    CRUD operation started in hibernate

---
+++ HibernateProject/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+//        s1.setRollNo(23);
+//        s1.setsName("Murari");
+//        s1.setsAge(25);
+
+        //        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure();
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
+
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        s1 = session.find(Student.class,23);
+        session.remove(s1);
+        transaction.commit();
+
+        //fetching data
+//        Student s2 = session.find(Student.class,4);
+//        sf.close();
+        System.out.println(s1);
+
+
+    }
+}
