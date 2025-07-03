ef15fa8a7799fa9d62b67c345ed4cd88440cac08
Ritwik <ritwikhero@gmail.com>
2025-07-04 02:12:28 +0530

    Changing table name & column name using hibernate ORM

--- HibernateProject/src/main/java/org/example/Main.java
+++ HibernateProject/src/main/java/org/example/Main.java
@@ -9,31 +9,49 @@ import org.hibernate.cfg.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student();
-//        s1.setRollNo(23);
-//        s1.setsName("Murari");
-//        s1.setsAge(25);
+//        Student s1 = new Student();
+////        s1.setRollNo(23);
+////        s1.setsName("Murari");
+////        s1.setsAge(25);
+//
+//        //        Configuration cfg = new Configuration();
+////        cfg.addAnnotatedClass(org.example.Student.class);
+////        cfg.configure();
+//
+//        SessionFactory sf = new Configuration()
+//                .addAnnotatedClass(org.example.Student.class)
+//                .configure()
+//                .buildSessionFactory();
+//
+//        Session session = sf.openSession();
+//        Transaction transaction = session.beginTransaction();
+//        s1 = session.find(Student.class,23);
+//        session.remove(s1);
+//        transaction.commit();
+//
+//        //fetching data
+////        Student s2 = session.find(Student.class,4);
+////        sf.close();
+//        System.out.println(s1);
 
-        //        Configuration cfg = new Configuration();
-//        cfg.addAnnotatedClass(org.example.Student.class);
-//        cfg.configure();
+        Alien  a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Ritwik");
+        a1.setTech("Java");
 
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(org.example.Student.class)
+                .addAnnotatedClass(org.example.Alien.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
         Transaction transaction = session.beginTransaction();
-        s1 = session.find(Student.class,23);
-        session.remove(s1);
+
+        session.persist(a1);
         transaction.commit();
 
-        //fetching data
-//        Student s2 = session.find(Student.class,4);
-//        sf.close();
-        System.out.println(s1);
-
+        session.close();
+        sf.close();
 
     }
 }
