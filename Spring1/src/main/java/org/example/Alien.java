3bd6d141dcab6a7ef0a3b0157c21f09978c621cf
Ritwik <ritwikhero@gmail.com>
2025-07-08 00:33:08 +0530

    understanding spring

---
+++ Spring1/src/main/java/org/example/Alien.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Alien {
+
+    int age ;
+
+    public Alien(){
+        System.out.println("object created");
+    }
+
+    public void code(){
+        System.out.println("Coding....");
+    }
+}
---
+++ Spring1/src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Alien a1 = (Alien) context.getBean("alien1");
+        a1.age = 21;
+        a1.code();
+        System.out.println(a1.age);
+
+
+        Alien a2 = (Alien) context.getBean("alien1");
+        a2.code();
+        System.out.println(a2.age);
+    }
+}
