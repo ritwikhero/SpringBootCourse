8bfd60fb4d20230e03e6a3cd1ca0d45ebadde0a2
Ritwik <ritwikhero@gmail.com>
2025-07-09 00:34:07 +0530

    constructor injection

--- Spring1/src/main/java/org/example/Alien.java
+++ Spring1/src/main/java/org/example/Alien.java
@@ -1,8 +1,30 @@
 package org.example;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
 
     private int age ;
+    private int salary;
+
+    @ConstructorProperties({"age", "salary", "lap" })
+    public Alien(int age, int salary, Laptop lap) {
+        this.age = age;
+        this.salary = salary;
+        this.lap = lap;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+//    public Alien(int age, Laptop lap) {
+//        this.age = age;
+//        this.lap = lap;
+//    }
 
     private Laptop lap;
 
