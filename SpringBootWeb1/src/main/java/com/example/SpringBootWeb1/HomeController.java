c99d168cc47cbd1fec58ab2daf80bb23cab51bc9
Ritwik <ritwikhero@gmail.com>
2025-07-19 02:49:27 +0530

    result now getting displayed

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,7 +1,11 @@
 package com.example.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
 
 @Controller
 public class HomeController {
@@ -11,4 +15,16 @@ public class HomeController {
         System.out.println("home method called");
         return "index.jsp";
     }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession session){
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1+num2;
+        session.setAttribute("result",result);
+        System.out.println("result  is: "+result);
+        return "result.jsp";
+    }
+
 }
