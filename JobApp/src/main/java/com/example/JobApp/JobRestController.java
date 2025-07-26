51c5544d80ac81f9ac36aa4883e954ce040caf61
Ritwik <ritwikhero@gmail.com>
2025-07-27 01:05:01 +0530

    SpringData JPA started

--- JobApp/src/main/java/com/example/JobApp/JobRestController.java
+++ JobApp/src/main/java/com/example/JobApp/JobRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-public class JobController {
+public class JobRestController {
     @Autowired
     private JobService service;
 
