17414eb3f55e1851795181094244e9f16bde4b4e
Ritwik <ritwikhero@gmail.com>
2025-08-04 01:58:30 +0530

    Spring data rest project added

---
+++ spring-data-rest-demo/src/main/java/com/example/spring_data_rest_demo/model/JobPost.java
@@ -0,0 +1,25 @@
+package com.example.spring_data_rest_demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
+public class JobPost {
+    @Id
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private Integer reqExperience;
+    private List<String> postTechStack;
+
+}
---
+++ spring-data-rest-demo/src/main/java/com/example/spring_data_rest_demo/repo/JobRepo.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_rest_demo.repo;
+
+import com.example.spring_data_rest_demo.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+    List<JobPost> searchByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+}
