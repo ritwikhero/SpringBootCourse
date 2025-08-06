bc03c41f50e53e5de1462835925003f2e6388ce5
Ritwik <ritwikhero@gmail.com>
2025-08-07 00:56:48 +0530

    performance monitoring aspect started

---
+++ Spring-boot-rest/src/main/java/com/example/Spring_boot_rest/aop/PerformanceMointorAspect.java
@@ -0,0 +1,10 @@
+package com.example.Spring_boot_rest.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerformanceMointorAspect {
+
+}
