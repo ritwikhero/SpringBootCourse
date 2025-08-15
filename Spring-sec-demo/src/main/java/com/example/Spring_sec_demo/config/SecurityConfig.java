36903db8daa024cbe1c1e7d1b010b34d3f8a4c74
Ritwik <ritwikhero@gmail.com>
2025-08-15 17:09:17 +0530

    security config finished

--- Spring-sec-demo/src/main/java/com/example/Spring_sec_demo/config/SecurityConfig.java
+++ Spring-sec-demo/src/main/java/com/example/Spring_sec_demo/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.Spring_sec_demo.config;
 
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +15,39 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+//
+//        http.csrf(customizer -> customizer.disable());
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+
+        /*
+         * Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new
+         * Customizer<CsrfConfigurer<HttpSecurity>>() {
+         *
+         * @Override public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+         *
+         * configurer.disable(); } };
+         *
+         * Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.
+         * AuthorizationManagerRequestMatcherRegistry> custHttp = new
+         * Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.
+         * AuthorizationManagerRequestMatcherRegistry>() {
+         *
+         * @Override public void customize(
+         * AuthorizeHttpRequestsConfigurer<HttpSecurity>.
+         * AuthorizationManagerRequestMatcherRegistry registry) {
+         * registry.anyRequest().authenticated();
+         *
+         * } };
+         *
+         * http.authorizeHttpRequests(custHttp); http.csrf(custCsrf);
+         */
     }
 }
