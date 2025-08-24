f00d1eb315076de125be99c03e263fc1b99b8d5a
Ritwik <ritwikhero@gmail.com>
2025-08-25 00:08:26 +0530

    jwt work in progress

--- Spring-sec-demo/src/main/java/com/example/Spring_sec_demo/config/SecurityConfig.java
+++ Spring-sec-demo/src/main/java/com/example/Spring_sec_demo/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomEditorConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,7 +48,7 @@ public class SecurityConfig {
 //        http.httpBasic(Customizer.withDefaults());
 //        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.requestMatchers("register").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request.requestMatchers("register","login").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
@@ -73,6 +75,10 @@ public class SecurityConfig {
          *
          * http.authorizeHttpRequests(custHttp); http.csrf(custCsrf);
          */
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration cofig) throws Exception {
+        return cofig.getAuthenticationManager();
     }
     @Bean
     public UserDetailsService userDetailsService(){
