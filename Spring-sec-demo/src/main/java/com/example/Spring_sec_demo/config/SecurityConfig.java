aa1d9d803be7acaa8b919d57102b1e310d55abcf
Ritwik <ritwikhero@gmail.com>
2025-08-31 01:34:51 +0530

    Token used for login now

--- Spring-sec-demo/src/main/java/com/example/Spring_sec_demo/config/SecurityConfig.java
+++ Spring-sec-demo/src/main/java/com/example/Spring_sec_demo/config/SecurityConfig.java
@@ -1,7 +1,112 @@
 package com.example.Spring_sec_demo.config;
 
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.config.CustomEditorConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private JwtFilter jwtFilter;
+//
+//    @Bean
+//    public AuthenticationProvider authProvider() {
+//        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(userDetailsService);
+////        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+//
+//        return provider;
+//    }
+//
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////
+////        http.csrf(customizer -> customizer.disable());
+////        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//////        http.formLogin(Customizer.withDefaults());
+////        http.httpBasic(Customizer.withDefaults());
+////        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.csrf(customizer -> customizer.disable())
+//                .authorizeHttpRequests(request -> request.requestMatchers("register","login").permitAll().anyRequest().authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//        return http.build();
+//
+//        /*
+//         * Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new
+//         * Customizer<CsrfConfigurer<HttpSecurity>>() {
+//         *
+//         * @Override public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//         *
+//         * configurer.disable(); } };
+//         *
+//         * Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.
+//         * AuthorizationManagerRequestMatcherRegistry> custHttp = new
+//         * Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.
+//         * AuthorizationManagerRequestMatcherRegistry>() {
+//         *
+//         * @Override public void customize(
+//         * AuthorizeHttpRequestsConfigurer<HttpSecurity>.
+//         * AuthorizationManagerRequestMatcherRegistry registry) {
+//         * registry.anyRequest().authenticated();
+//         *
+//         * } };
+//         *
+//         * http.authorizeHttpRequests(custHttp); http.csrf(custCsrf);
+//         */
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration cofig) throws Exception {
+//        return cofig.getAuthenticationManager();
+//    }
+////    @Bean
+////    public UserDetailsService userDetailsService(){
+////
+////        UserDetails user = User
+////                .withDefaultPasswordEncoder()
+////                .username("ritwik")
+////                .password("r@123")
+////                .roles("USER")
+////                .build();
+////
+////        UserDetails admin = User
+////                .withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("a@123")
+////                .roles("ADMIN")
+////                .build();
+////
+////
+////        return new InMemoryUserDetailsManager(user,admin);
+////    }
+//}
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.CustomEditorConfigurer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,13 +117,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,77 +130,51 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-//        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
-
         return provider;
     }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http.csrf(customizer -> customizer.disable());
-//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-////        http.formLogin(Customizer.withDefaults());
-//        http.httpBasic(Customizer.withDefaults());
-//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.requestMatchers("register","login").permitAll().anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
 
-        /*
-         * Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new
-         * Customizer<CsrfConfigurer<HttpSecurity>>() {
-         *
-         * @Override public void customize(CsrfConfigurer<HttpSecurity> configurer) {
-         *
-         * configurer.disable(); } };
-         *
-         * Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.
-         * AuthorizationManagerRequestMatcherRegistry> custHttp = new
-         * Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.
-         * AuthorizationManagerRequestMatcherRegistry>() {
-         *
-         * @Override public void customize(
-         * AuthorizeHttpRequestsConfigurer<HttpSecurity>.
-         * AuthorizationManagerRequestMatcherRegistry registry) {
-         * registry.anyRequest().authenticated();
-         *
-         * } };
-         *
-         * http.authorizeHttpRequests(custHttp); http.csrf(custCsrf);
-         */
+        http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register", "login")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
+
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration cofig) throws Exception {
-        return cofig.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//
-//        UserDetails user = User
-//                .withDefaultPasswordEncoder()
-//                .username("ritwik")
-//                .password("r@123")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User
-//                .withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("a@123")
-//                .roles("ADMIN")
-//                .build();
-//
-//
-//        return new InMemoryUserDetailsManager(user,admin);
-//    }
+
+
+
+
+    /*
+     * @Bean public UserDetailsService userDetailsService() {
+     *
+     * UserDetails user=User .withDefaultPasswordEncoder() .username("navin")
+     * .password("n@123") .roles("USER") .build();
+     *
+     * UserDetails admin=User .withDefaultPasswordEncoder() .username("admin")
+     * .password("admin@789") .roles("ADMIN") .build();
+     *
+     * return new InMemoryUserDetailsManager(user,admin); }
+     */
+
+
 }
